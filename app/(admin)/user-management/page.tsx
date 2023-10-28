8e3af3765aa5e30ca4a503d9ff8ed336e22b4568
Celeste <aafree2014@gmail.com>
2023-10-28 23:02:58 +0800

    加入 selected row 功能

--- app/(admin)/user-management/page.tsx
+++ app/(admin)/user-management/page.tsx
@@ -1,95 +1,9 @@
-import { ReactNode } from "react";
 import Stat from "./components/Stat";
-import Table from "@COM/(admin)/utils/Table";
-import Dropdown from "./components/Dropdown";
+import UserTable from "./components/UserTable";
 
 type Props = {};
 
-type Columns = {
-  title: string | ReactNode;
-  key: string;
-  dataIndex: string;
-  render?: (text: any, row: any) => ReactNode;
-  align?: "left" | "center" | "right";
-};
-
-type User = {
-  id: string;
-  name: string;
-  imageUrl: string;
-  isActive: boolean;
-  createdAt: Date;
-  role: string;
-};
-
 const page = (props: Props) => {
-  const userList: User[] = [
-    {
-      id: "1211",
-      name: "Celeste",
-      imageUrl: "41541",
-      isActive: true,
-      createdAt: new Date("2023-10-24"),
-      role: "管理員",
-    },
-  ];
-
-  const columns: Array<Columns> = [
-    {
-      title: <input type="checkbox" className="checkbox checkbox-primary" />,
-      dataIndex: "id",
-      key: "id",
-      align: "center",
-      render: (text: any, row: any) => (
-        <input type="checkbox" className="checkbox checkbox-primary" />
-      ),
-    },
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-      align: "center",
-    },
-    {
-      title: "姓名",
-      dataIndex: "name",
-      key: "name",
-      align: "center",
-    },
-    {
-      title: "圖片",
-      dataIndex: "imageUrl",
-      align: "center",
-      key: "imageUrl",
-    },
-    {
-      title: "是否啟用",
-      dataIndex: "isActive",
-      align: "center",
-      key: "isActive",
-      render: (isActive: boolean, row: any) => (
-        <input
-          type="checkbox"
-          checked={isActive}
-          className="checkbox checkbox-primary"
-        />
-      ),
-    },
-    {
-      title: "角色",
-      dataIndex: "role",
-      align: "center",
-      key: "role",
-    },
-    {
-      title: "功能",
-      dataIndex: "operation",
-      key: "operation",
-      align: "center",
-      render: (_, row) => <Dropdown data={row} />,
-    },
-  ];
-
   return (
     <div className="mt-5 rounded-lg border p-5 min-h-[600px] relative user-table">
       <div className="grid grid-col-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
@@ -98,8 +12,8 @@ const page = (props: Props) => {
         <Stat mainNumber={8} desc="active" />
         <Stat mainNumber={8} desc="total" />
       </div>
-      <div className="overflow-x-auto mt-7 mb-3 border rounded-xl ">
-        <Table className="text-base" columns={columns} dataSource={userList} />
+      <div>
+        <UserTable />
       </div>
     </div>
   );
--- components/(admin)/utils/Table.tsx
+++ components/(admin)/utils/Table.tsx
@@ -1,5 +1,8 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, MouseEvent, createContext, useContext } from "react";
 import { twMerge } from "tailwind-merge";
+
+const TableContext = createContext<string[] | undefined>(undefined);
 
 type BasicTable<T> = {
   key: string; // unique key
@@ -8,7 +11,7 @@ type BasicTable<T> = {
 
 type BasicTableColumns<T> = {
   title: string | ReactNode;
-  dataIndex: keyof T;
+  dataIndex: string;
   align?: "left" | "center" | "right";
   render?: (text: any, row: T) => ReactNode;
 } & BasicTable<T>;
@@ -17,45 +20,59 @@ type Props<T extends BasicTable<T>> = {
   columns: Array<BasicTableColumns<T>>;
   dataSource: Array<T>;
   className: string;
+  onRow?: (e: MouseEvent<HTMLElement>, row: T) => void;
+  deletedRowList?: Array<string>;
 };
 
 export default function Table<T extends BasicTable<T>>({
   columns,
   dataSource,
   className,
+  onRow,
+  deletedRowList,
 }: Props<T>) {
   return (
-    <table className={twMerge("table", className)}>
-      {/* head */}
-      <thead>
-        {columns.map((column, index) => (
-          <th key={column.key} className="text-base border-b">
-            <span
-              className={twMerge(
-                column.align ? `flex justify-${column.align}` : ""
-              )}
-            >
-              {column.title}
-            </span>
-          </th>
-        ))}
-      </thead>
-      <tbody>
-        {dataSource.map((data, index) => (
-          <TableRow key={data.key} data={data} columns={columns} />
-        ))}
-      </tbody>
-    </table>
+    <TableContext.Provider value={deletedRowList}>
+      <table className={twMerge("table", className)}>
+        {/* head */}
+        <thead>
+          {columns.map((column, index) => (
+            <th key={column.key} className="text-base border-b">
+              <span
+                className={twMerge(
+                  column.align ? `flex justify-${column.align}` : ""
+                )}
+              >
+                {column.title}
+              </span>
+            </th>
+          ))}
+        </thead>
+        <tbody>
+          {dataSource.map((data, index) => (
+            <TableRow
+              key={data.key}
+              data={data}
+              columns={columns}
+              onRow={onRow}
+            />
+          ))}
+        </tbody>
+      </table>
+    </TableContext.Provider>
   );
 }
 
 function TableRow<T extends BasicTable<T>>({
   data,
   columns,
+  onRow,
 }: {
   data: T;
   columns: BasicTableColumns<T>[];
+  onRow?: (e: MouseEvent<HTMLElement>, row: T) => void;
 }) {
+  const context = useContext(TableContext);
   let components: Array<ReactNode> = [];
   const getTableCell = (data: T) => {
     columns.forEach((column) => {
@@ -75,7 +92,10 @@ function TableRow<T extends BasicTable<T>>({
   };
   getTableCell(data);
   return (
-    <tr className="border-b last:border-0">
+    <tr
+      className={twMerge("border-b last:border-0", context?.includes(data.key)? "bg-primary bg-opacity-10" : "")}
+      onClick={(e) => onRow?.(e, data)}
+    >
       {components.map((component) => component)}
     </tr>
   );
